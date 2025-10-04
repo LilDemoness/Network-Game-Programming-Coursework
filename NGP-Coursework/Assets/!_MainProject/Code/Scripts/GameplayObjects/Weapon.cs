e41d0fce3405ae65bb7f40ae5809165f2d7d67ce
LilDemoness <were.t.rex11@gmail.com>
2025-10-04 08:25:37 +0100

    Weapon Firing from Child Transform
    We've made it so that you can now use a child transform of a Weapon to specify it's firing direction, letting us account for changes in the gun's rotation without needing to recalculate our conversionMatrix.

--- NGP-Coursework/Assets/!_MainProject/Code/Scripts/GameplayObjects/Weapon.cs
+++ NGP-Coursework/Assets/!_MainProject/Code/Scripts/GameplayObjects/Weapon.cs
@@ -10,8 +10,7 @@ namespace Gameplay.GameplayObjects
         public WeaponData WeaponData => m_weaponData;
 
 
-        [SerializeField] private Vector3 _firingPosition = Vector3.zero;
-        [SerializeField] private Vector3 _firingDirection = Vector3.forward;
+        [SerializeField] private Transform _firingOrigin;
         private NetworkObject _parentNetworkObject;
         private Matrix4x4 conversionMatrix;
 
@@ -31,15 +30,36 @@ namespace Gameplay.GameplayObjects
 
 
         public ulong GetAttackOriginTransformID() => _parentNetworkObject.NetworkObjectId;
-        public Vector3 GetAttackLocalOffset() => conversionMatrix.MultiplyPoint(_firingPosition);
-        public Vector3 GetAttackLocalDirection() => conversionMatrix.MultiplyPoint(_firingDirection).normalized;
+        public Vector3 GetAttackLocalOffset() => conversionMatrix.MultiplyPoint(_firingOrigin.localPosition);
+        public Vector3 GetAttackLocalDirection() => conversionMatrix.MultiplyPoint(_firingOrigin.localRotation * Vector3.forward).normalized;
 
 
-        private void OnDrawGizmosSelected()
+        #if UNITY_EDITOR
+        private bool Editor_IsThisOrChildSelected()
         {
-            Gizmos.color = Color.red;
-            Gizmos.DrawSphere(transform.TransformPoint(_firingPosition), 0.05f);
-            Gizmos.DrawRay(transform.TransformPoint(_firingPosition), transform.TransformDirection(_firingDirection));
+            Transform selectedTransform = UnityEditor.Selection.activeTransform;
+            while(selectedTransform != null)
+            {
+                if (selectedTransform == this.transform)
+                    return true;
+
+                selectedTransform = selectedTransform.parent;
+            }
+
+            return false;
         }
+
+        private void OnDrawGizmos()
+        {
+            if (_firingOrigin == null)
+                return;
+            if (!Editor_IsThisOrChildSelected())
+                return;
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(transform.TransformPoint(_firingOrigin.localPosition), 0.05f);
+            Gizmos.DrawRay(transform.TransformPoint(_firingOrigin.localPosition), transform.TransformDirection(_firingOrigin.localRotation * Vector3.forward));
+        }
+        #endif
     }
 }
