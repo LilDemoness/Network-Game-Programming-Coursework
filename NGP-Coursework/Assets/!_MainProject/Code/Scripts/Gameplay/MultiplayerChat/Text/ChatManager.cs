7861b72198bd212e0f9b676c2e369ac37248c065
LilDemoness <were.t.rex11@gmail.com>
2025-12-09 00:15:52 +0000

    Cheats Instance Check & Smallcaps Disable
    We've implemented an instance check for the DebugCheatManager in the ChatManager so that it only attempts to process a cheat if the DebugCheatManager exists (During Gameplay), preventing errors from doing so, and we notify the triggering client if this occurs.
    
    We also disabled the Smallcaps option for the chat to make it easier to differentiate between Uppercase and Lowercase letters.

--- NGP-Coursework/Assets/!_MainProject/Code/Scripts/Gameplay/MultiplayerChat/Text/ChatManager.cs
+++ NGP-Coursework/Assets/!_MainProject/Code/Scripts/Gameplay/MultiplayerChat/Text/ChatManager.cs
@@ -57,10 +57,11 @@ namespace Gameplay.MultiplayerChat.Text
         {
             if (_isCapturingInput)
                 return; // Chat Input is already open.
+            _isCapturingInput = true;   // Mark the chat as open.
 
+            // Set chat visibility.
             SetChatInputVisibility(true);
             StopMainTextChatFade();
-            _isCapturingInput = true;
 
             // Subscribe to Submission Events.
             ClientInput.OnSubmitChatPerformed += SubmitChat;
@@ -69,7 +70,7 @@ namespace Gameplay.MultiplayerChat.Text
             // Select the Input Field.
             // We are deselecting and then reselecting the next frame to prevent an error with having the input field selected but the user being unable to interact with it.
             EventSystem.current.SetSelectedGameObject(null);
-            StartCoroutine(Test());
+            StartCoroutine(ReselectInputAfterFrame());
 
             // Prevent Unrelated Input.
             ClientInput.PreventActions(typeof(ChatManager), ALL_ACTIONS_BUT_CHAT);
@@ -78,10 +79,10 @@ namespace Gameplay.MultiplayerChat.Text
         {
             if (!_isCapturingInput)
                 return; // Chat Input is already closed.
+            _isCapturingInput = false;  // Mark the chat as closed.
 
             SetChatInputVisibility(false);
             StartMainTextChatFade();
-            _isCapturingInput = false;
 
             // Unsubscribe from Submission Events.
             ClientInput.OnSubmitChatPerformed -= SubmitChat;
@@ -96,18 +97,27 @@ namespace Gameplay.MultiplayerChat.Text
             _chatInputCanvasGroup.blocksRaycasts = isVisible;
         }
 
-        private IEnumerator Test() { yield return null; EventSystem.current.SetSelectedGameObject(_chatInput.gameObject); }
+        private IEnumerator ReselectInputAfterFrame() { yield return null; EventSystem.current.SetSelectedGameObject(_chatInput.gameObject); }
 
         #endregion
 
+        /// <summary>
+        ///     Submit the contents of '_chatInput' for processing and close the chat input window.
+        /// </summary>
         public void SubmitChat()
         {
             SendChatMessage();
             CloseChatInput();
         }
+        /// <summary>
+        ///     Close the current chat input without clearing or processing the contents of '_chatInput'
+        /// </summary>
         public void CancelChat() => CloseChatInput();
 
 
+        /// <summary>
+        ///     Process the contents of the '_chatInput' InputField and, if valid, send to the server for relaying to other clients.
+        /// </summary>
         public void SendChatMessage()
         {
             if (string.IsNullOrEmpty(_chatInput.text))
@@ -124,27 +134,44 @@ namespace Gameplay.MultiplayerChat.Text
 
             if (Gameplay.DebugCheats.DebugCheatManager.IsCommand(_chatInput.text))
             {
-                Gameplay.DebugCheats.DebugCheatManager.Instance.PerformCheat(_chatInput.text);
-                _chatInput.text = "";
+                // Our input is a cheat.
+                if (Gameplay.DebugCheats.DebugCheatManager.HasInstance)
+                    Gameplay.DebugCheats.DebugCheatManager.Instance.PerformCheat(_chatInput.text);  // Instance Exists: Perform Cheat as Normal.
+                else
+                    ReceiveChatMessage(null, "Cheats Unavailable while DebugCheatManager is Uninitialised. Try again during gameplay"); // No Instance: Warn the user.
+
+                _chatInput.text = "";   // Clear the input box for the next message.
                 return;
             }
 
 #endif
-
+            // Send our message to the server which will then notify all clients.
             SendChatMessageServerRpc(_persistentPlayer.NetworkNameState.Name.Value, _chatInput.text);
-            _chatInput.text = "";
+            _chatInput.text = "";   // Clear the input box for the next message.
         }
+        /// <summary>
+        ///     Send a chat message directly without any checks, using the supplied name & message contents strings.
+        /// </summary>
         public void SendChatMessage(string name, string message)
         {
             SendChatMessageServerRpc(name ?? "", message);
             _chatInput.text = "";
         }
 
+        /// <summary>
+        ///     Send a Chat Message to the Server to be Processed and Relayed to Clients.
+        /// </summary>
         [Rpc(SendTo.Server)]
         private void SendChatMessageServerRpc(FixedPlayerName senderName, string message) => ReceiveChatMessageClientRpc(senderName, message);
+        /// <summary>
+        ///     Send a Chat Message to all Clients.
+        /// </summary>
         [Rpc(SendTo.ClientsAndHost)]
         private void ReceiveChatMessageClientRpc(FixedPlayerName senderName, string message) => ReceiveChatMessage(senderName, message);
 
+        /// <summary>
+        ///     Process a received Chat Message on this client.
+        /// </summary>
         public void ReceiveChatMessage(string name, string message)
         {
             AddMessage(name, message);
@@ -152,6 +179,9 @@ namespace Gameplay.MultiplayerChat.Text
         }
 
 
+        /// <summary>
+        ///     Instantiate & Setup a Chat Message
+        /// </summary>
         private void AddMessage(string name, string message)
         {
             ChatMessage chatMessage = Instantiate<ChatMessage>(_chatMessagePrefab, _chatMessageContainer);
@@ -161,11 +191,17 @@ namespace Gameplay.MultiplayerChat.Text
 
         #region Main Chat Fading
 
+        /// <summary>
+        ///    Fade the main text chat after a short delay.
+        /// </summary>
         private void StartMainTextChatFade()
         {
             StopMainTextChatFade();
             _fadeMainTextChatCoroutine = StartCoroutine(FadeMainTextChat());
         }
+        /// <summary>
+        ///    Stop the current fade of main text chat and set its alpha to 1.
+        /// </summary>
         private void StopMainTextChatFade()
         {
             if (_fadeMainTextChatCoroutine != null)
@@ -176,6 +212,9 @@ namespace Gameplay.MultiplayerChat.Text
 
         private readonly WaitForSeconds WAIT_TO_START_FADING = new WaitForSeconds(4.0f);
         private const float FADE_DURATION = 0.5f;
+        /// <summary>
+        ///    Fade the main text chat after a short delay.
+        /// </summary>
         private IEnumerator FadeMainTextChat()
         {
             yield return WAIT_TO_START_FADING;
